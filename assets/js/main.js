4ceca1cae64cfcab2dfea99563f1997364933e8a
erwinjames <37238531+erwinjames@users.noreply.github.com>
2020-08-22 22:00:42 +0800

    Add files via upload
    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('sections[id]')
+
+window.addEventListener('scroll',
+    scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', { dealy: 200 })
+sr.reveal('.home__img', { origin: 'right', delay: 400 })
+
+sr.reveal('.about__img', { delay: 500 })
+sr.reveal('.about__subtitle', { delay: 300 })
+sr.reveal('.about__profession', { delay: 400 })
+sr.reveal('.about__text', { delay: 500 })
+sr.reveal('.about__social', { delay: 600, interval: 200 })
+
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
+sr.reveal('.skills__img', { delay: 400 })
+
+sr.reveal('.portfolio__img', { interval: 200 })
+
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', { interval: 200 })
+sr.reveal('.contact__input', { delay: 400 })
+sr.reveal('.contact__button', { delay: 600 })
