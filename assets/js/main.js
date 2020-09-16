16d71baec7cb0a3b79851113945d66660814108a
erwinjames <manugasewinjames@gmail.com>
2020-09-16 17:45:12 +0800

    adding modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e) {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+
+        $('[data-popup=" ' + targeted_popup_class + ' "]').fadeIn(350);
+
+        e.preventDefault();
+    });
+
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e) {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup=" ' + targeted_popup_class + ' "]').fadeOut(350);
+
+        e.preventDefault();
+    });
+});
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
