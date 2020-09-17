b0d181219533189790b50aa9ec5e6287fb9265e4
erwinjames <manugasewinjames@gmail.com>
2020-09-17 23:51:22 +0800

    iniital commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(function() {
-
     $('[data-popup-open]').on('click', function(e) {
         var targeted_popup_class = jQuery(this).attr('data-popup-open');
         $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
